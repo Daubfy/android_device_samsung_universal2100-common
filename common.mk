7fe8fe490b0bfc8e21a7395b575e4c44a6dce0c2
Lokesh Gidra <lokeshgidra@google.com>
2019-12-06 13:19:19 -0800

    exynos2100: Setup 6GB Dalvik heap configuration
    Change-Id: Ia2b826c95643f935878335c2434deadf7f8401c4

--- common.mk
+++ common.mk
@@ -6,6 +6,9 @@
 
 COMMON_PATH := device/samsung/exynos2100
 
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
 # API
 PRODUCT_SHIPPING_API_LEVEL := 30
 BOARD_SHIPPING_API_LEVEL := 30

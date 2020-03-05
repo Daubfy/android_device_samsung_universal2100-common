1942997793ce74bd800e104f51ee4862577ee60a
LuK1337 <priv.luk@gmail.com>
2020-03-05 12:03:42 +0100

    exynos2100: Set ro.netflix.bsp_rev
    * This prop makes Netflix select L1 instead of L3.
    
    Change-Id: Idc0368803ca082e99392fb136f2cdc41a4cb0c87

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -125,6 +125,7 @@ DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 
 ## Properties
+TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 
 ## Recovery

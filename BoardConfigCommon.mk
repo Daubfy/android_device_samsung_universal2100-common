f629a4550d5602755762af4479a76e3d0e021d77
Tim Zimmermann <tim@linux4.de>
2022-05-15 06:00:04 +0200

    exynos2100: Add device FCM for samsung HALs
    Change-Id: I8e55e0356ca3abac073757a436c9eb7639a09b41

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -114,6 +114,8 @@ BOARD_ROOT_EXTRA_FOLDERS := efs
 
 ## Manifest
 # HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/device_framework_matrix.xml
+
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 

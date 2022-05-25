c76f67049279d47c3a0f906b0671f46c7ca16100
Josip Kelecic <kelecicj@gmail.com>
2022-05-25 14:31:13 +0200

    exynos2100: Add vendor properties from stock
    Signed-off-by: Mst8981 <unformed8981@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -57,6 +57,9 @@ BOARD_USES_METADATA_PARTITION := true
 
 BOARD_ROOT_EXTRA_FOLDERS := efs
 
+## Properties
+TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
+
 ## Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3

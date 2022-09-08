99ca2c72e5056cfa2cf0c89eb9fd0ae68a9868d0
Josip Kelecic <kelecicj@gmail.com>
2022-09-08 17:22:03 +0200

    exynos2100: Setup and build SoundTrigger HAL
    Change-Id: I265e5eceb5b3de8361852004229124a4ebda95c5
    Signed-off-by: ExtremeXT <extremextdev@gmail.com>

--- common.mk
+++ common.mk
@@ -35,6 +35,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl:32 \
     android.hardware.audio.service \
     android.hardware.audio.effect@7.0-impl:32 \
+    android.hardware.soundtrigger@2.3-impl:32 \
     audio.bluetooth.default \
     android.hardware.bluetooth.audio-impl \
     audio.r_submix.default \

3474fcf848fc6ee2b953a75d45ca842de2e3a2ce
LuK1337 <priv.luk@gmail.com>
2022-11-03 20:57:11 +0100

    exynos2100: Add hardware/samsung to soong namespaces
    See I78b1b9930a57bc01afda722b9f8a196c372f9d8b.
    
    Change-Id: I634b562e12b70a0c060d449d3ba1d4e87204cf5d

--- common.mk
+++ common.mk
@@ -47,6 +47,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/init/fstab.exynos2100:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.exynos2100
 
+# Soong Namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(COMMON_PATH) \
+    hardware/samsung
+
 # Vendor service manager
 PRODUCT_PACKAGES += \
     vndservicemanager

af05d7e7ecb2406348456bdab37a301d673ea63b
Tim Zimmermann <tim@linux4.de>
2024-01-11 17:46:44 +0300

    exynos2100: audio: Uprev audio HAL to V7
    Updated audio policy by running:
    
    hardware/interfaces/audio/7.0/config/update_audio_policy_config.sh \
    device/samsung/sm7325-common/audio/configs/audio_policy_configuration.xml 6.0
    
    with some manual intervention.
    
    Change-Id: Iede32c176742e89577365341732a86f6ecaee514
    Signed-off-by: Mst8981 <unformed8981@gmail.com>

--- common.mk
+++ common.mk
@@ -32,9 +32,9 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@6.0-impl:32 \
+    android.hardware.audio@7.0-impl:32 \
     android.hardware.audio.service \
-    android.hardware.audio.effect@6.0-impl:32 \
+    android.hardware.audio.effect@7.0-impl:32 \
     audio.bluetooth.default \
     android.hardware.bluetooth.audio-impl \
     audio.r_submix.default \
@@ -49,6 +49,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
+    frameworks/av/services/audiopolicy/config/bluetooth_with_le_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_with_le_audio_policy_configuration_7_0.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml

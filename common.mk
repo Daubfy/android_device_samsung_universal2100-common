4377fc70173a7b86136d1ba7db41a3b04d1e6a03
ata-kaner <ata.kaner03@gmail.com>
2024-04-08 09:15:27 +0300

    exynos2100: Inherit emulated_storage.mk
    With the deprecation of SdcardFS in kernel, we can now inherit emulated_storage without any problem.
    
    Signed-off-by: ata-kaner <ata.kaner03@gmail.com>

--- common.mk
+++ common.mk
@@ -9,6 +9,9 @@ COMMON_PATH := device/samsung/exynos2100
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
 # API
 PRODUCT_SHIPPING_API_LEVEL := 30
 BOARD_SHIPPING_API_LEVEL := 30

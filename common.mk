e6e843aa74784103e5a7a2e0dd9d5d262ed251e4
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-17 04:59:30 +0300

    exynos2100: Do not manually build dependencies
    Change-Id: I4046d8eeabfc70842898b7034f75cdd6bcfc441b

--- common.mk
+++ common.mk
@@ -272,10 +272,7 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.2.vendor \
-    android.hardware.secure_element@1.2.vendor \
     com.android.nfc_extras \
-    libchrome.vendor \
     nqnfcinfo \
     Tag
 

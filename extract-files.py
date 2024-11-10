5844dfa0f2a81fabf9b8f09419e1eae4a5974abe
borbelyvince <borbely.vince.11@gmail.com>
2024-11-10 11:18:18 +0100

    exynos2100: shim widevine with libcrypto_shim
    from hardware/lineage/compat
    
    Change-Id: I318fbbc32f850dede2c44bebc232f5da220ef4ab
    Signed-off-by: ExtremeXT <extremextdev@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -92,7 +92,8 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libcrypto.so', 'libcrypto-v33.so')
         .add_needed('libshim_crypto.so'),
     'vendor/lib/libwvhidl.so': blob_fixup()
-        .replace_needed('libprotobuf-cpp-lite-3.9.1.so', 'libprotobuf-cpp-full-3.9.1.so'),
+        .replace_needed('libprotobuf-cpp-lite-3.9.1.so', 'libprotobuf-cpp-full-3.9.1.so')
+        .add_needed('libcrypto_shim.so'),
     (
         'vendor/lib/libFilmGrainNoise.so',
         'vendor/lib64/libFilmGrainNoise.so',

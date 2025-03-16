a1cdc17069cbc5a7259e622f958e343e34f7dc59
Tim Zimmermann <tim@linux4.de>
2025-03-16 06:57:35 +0100

    exynos2100: Convert bcmdhd SAE offload flag to soong
    Change-Id: Ia1b0b08f4e21d2c98321dd04986bb814e3d0cb5a

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -159,7 +159,6 @@ BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 1
 BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
 
 ## Wifi related defines
-BOARD_WLAN_BCMDHD_SAE                         := true
 BOARD_WLAN_DEVICE                             := bcmdhd
 BOARD_WPA_SUPPLICANT_DRIVER                   := NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB              := lib_driver_cmd_bcmdhd
@@ -170,6 +169,8 @@ WIFI_FEATURE_HOSTAPD_11AX                     := true
 WIFI_HIDL_FEATURE_DUAL_INTERFACE              := true
 WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION                        := VER_0_8_X
+
+$(call soong_config_set,wpa_supplicant_8,board_wlan_bcmdhd_sae,true)
 
 ## Inherit proprietary vendor configuration
 include vendor/samsung/exynos2100/BoardConfigVendor.mk

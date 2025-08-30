532c9228a4e2ee131dde43190890fadc891f49a2
Yumi Yukimura <me.cafebabe@gmail.com>
2025-08-30 23:45:15 +0800

    exynos2100: Cleanup more flags
    Change-Id: I1169adf12d717417f5f328ebbcc01ecc9b911889

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -6,9 +6,6 @@
 
 COMMON_PATH := device/samsung/exynos2100
 
-BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
-BUILD_BROKEN_DUP_RULES := true
-
 ## Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a-dotprod

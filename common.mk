e182371f3ecc898df7b97be74b2734c7c598a414
Mst8981 <unformed8981@gmail.com>
2026-02-21 23:06:13 +0900

    exynos2100: enable SBWC DPU

--- common.mk
+++ common.mk
@@ -163,6 +163,7 @@ $(call soong_config_set,arm_gralloc,mali_gpu_support_afbc_basic,true)
 $(call soong_config_set,arm_gralloc,gralloc_init_afbc,true)
 $(call soong_config_set,arm_gralloc,gralloc_ion_sync_on_lock,true)
 $(call soong_config_set,arm_gralloc,dpu_support_1010102_afbc,true)
+$(call soong_config_set_bool,sbwcdpu,enabled,true)
 
 PRODUCT_PACKAGES += \
     android.hardware.vulkan.compute-0.prebuilt.xml \

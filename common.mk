b7a6b7722d4992d936a5adadaed6eb9973854aee
Atakan <ata.kaner03@gmail.com>
2026-05-31 16:57:58 +0300

    exynos2100: Build Lineage health HAL for Samsung
    Change-Id: Icbd08d8f74832a320c8e0b8c84518cdc996fa47e

--- common.mk
+++ common.mk
@@ -195,17 +195,7 @@ PRODUCT_PACKAGES += \
 
 # Lineage Health
 PRODUCT_PACKAGES += \
-    vendor.lineage.health-service.default
-
-$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/charging_enabled)
-$(call soong_config_set,lineage_health,charging_control_charging_enabled,1)
-$(call soong_config_set,lineage_health,charging_control_charging_disabled,0)
-$(call soong_config_set_bool,lineage_health,charging_control_charging_bypass,true)
-$(call soong_config_set_bool,lineage_health,charging_control_charging_toggle,true)
-$(call soong_config_set_bool,lineage_health,charging_control_charging_deadline,false)
-$(call soong_config_set,lineage_health,fast_charge_node,/sys/class/sec/switch/afc_disable)
-$(call soong_config_set,lineage_health,fast_charge_value_none,1)
-$(call soong_config_set,lineage_health,fast_charge_value_fast_charge,0)
+    vendor.lineage.health-service.samsung
 
 # Keymaster
 PRODUCT_PACKAGES += \

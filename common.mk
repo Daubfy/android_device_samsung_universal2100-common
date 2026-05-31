6cd72bbd731c938302e5e9f933def091d17fb9b3
Atakan <ata.kaner03@gmail.com>
2026-05-31 16:21:16 +0300

    exynos2100: Use bypass node for charging control
    Change-Id: Icbd08d8f74832a320c8e0b8c84518cdc996fa47e

--- common.mk
+++ common.mk
@@ -197,9 +197,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
-$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/batt_slate_mode)
-$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
-$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/charging_enabled)
+$(call soong_config_set,lineage_health,charging_control_charging_enabled,1)
+$(call soong_config_set,lineage_health,charging_control_charging_disabled,0)
 $(call soong_config_set_bool,lineage_health,charging_control_charging_bypass,true)
 $(call soong_config_set_bool,lineage_health,charging_control_charging_toggle,true)
 $(call soong_config_set_bool,lineage_health,charging_control_charging_deadline,false)

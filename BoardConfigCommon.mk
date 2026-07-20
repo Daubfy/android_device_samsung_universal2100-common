067b7a9125b11877021ff5d76815670057e01dd0
mst8981 <unformed8981@gmail.com>
2026-07-21 06:37:46 +0900

    exynos2100: reorganize VINTF files
    Move the VINTF manifest and compatibility matrix files
    under vintf/ to improve source tree organization

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -127,11 +127,11 @@ BOARD_ROOT_EXTRA_FOLDERS := efs
 ## Manifest
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-    $(COMMON_PATH)/device_framework_matrix.xml \
+    $(COMMON_PATH)/vintf/device_framework_matrix.xml \
     hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml
 
-DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
-DEVICE_MATRIX_FILE += $(COMMON_PATH)/compatibility_matrix.xml
+DEVICE_MANIFEST_FILE += $(COMMON_PATH)/vintf/manifest.xml
+DEVICE_MATRIX_FILE += $(COMMON_PATH)/vintf/compatibility_matrix.xml
 
 ## Properties
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
